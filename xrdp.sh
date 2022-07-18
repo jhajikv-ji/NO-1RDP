03a41d507bee2432094af002a6ff0fada7f31ed2
jhajikv-ji <107477338+jhajikv-ji@users.noreply.github.com>
2022-07-18 11:55:04 +0530

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,4 +1,5 @@
-sudo apt update
+sudo apt update 
+printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
 while :; do cat cat.txt ; sleep 10 ; done &           
 sudo apt install xrdp gnome-session 
 sudo service xrdp restart
