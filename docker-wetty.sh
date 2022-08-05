f6989990956bf400d0fa97f41d53a1df68f38152
jhajikv-ji <107477338+jhajikv-ji@users.noreply.github.com>
2022-08-06 03:38:39 +0530

    Update docker-wetty.sh

--- docker-wetty.sh
+++ docker-wetty.sh
@@ -4,8 +4,10 @@ docker run  -d -p 3000:3000 wettyoss/wetty --ssh-host=172.17.0.1
 sleep 10
 wget -O cli https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64  
 chmod +x ./cli && ./cli --url http://localhost:3000/wetty &>> cat.txt &
-while :; do cat cat.txt | sed '5!d' ; sleep 3 ; done
- 
+while :; do cat cat.txt | sed '5!d' | sed 's:[2022]*:[&:' |  sed 's:https*:](&:' |  sed 's:trycloudflare.com*:&/wetty):' ; sleep 3 ; done 
+
+
+
 
 ##. curl https://raw.githubusercontent.com/jhajikv-ji/no/main/docker-wetty.sh | bash
 
